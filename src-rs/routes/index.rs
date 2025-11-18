c8311521ac69b9b86f9da550e4a6c39495e2fa00
RickyFirmansyah27 <ricky_firmansyah@teknokrat.ac.id>
2025-11-18 11:16:15 +0700

    initial commit

--- src-rs/routes/index.rs
+++ src-rs/routes/index.rs
@@ -1,14 +1,16 @@
-use actix_web::{web, Scope};
+use actix_web::web;
 use crate::controllers::{
     greeting_controller::greeting,
     hello_controller::hello,
     user_controller::{create_user, get_user},
 };
 
-pub fn create_routes() -> Scope {
-    web::scope("")
-        .route("/", web::get().to(greeting))
-        .route("/hello", web::get().to(hello))
-        .route("/users", web::post().to(create_user))
-        .route("/users/{id}", web::get().to(get_user))
+pub fn configure_routes(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::scope("")
+            .route("/", web::get().to(greeting))
+            .route("/hello", web::get().to(hello))
+            .route("/users", web::post().to(create_user))
+            .route("/users/{id}", web::get().to(get_user))
+    );
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rust_app::routes::create_routes;
+use rust_app::routes::configure_routes;
 use rust_app::helpers::logger::init_logger;
 use actix_web::{App, HttpServer, middleware::Logger};
 
@@ -7,14 +7,11 @@ async fn main() -> std::io::Result<()> {
     // initialize tracing
     init_logger();
 
-    // build our application with routes
-    let app = create_routes();
-
     // run our app with actix web
     tracing::debug!("listening on 0.0.0.0:8080");
-    HttpServer::new(move || {
+    HttpServer::new(|| {
         App::new()
-            .configure(app)
+            .configure(configure_routes)
             .wrap(Logger::default())
     })
     .bind("0.0.0.0:8080")?
