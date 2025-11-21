8c293e707d3341ea5fcfebb547f43220d390f877
RickyFirmansyah27 <ricky_firmansyah@teknokrat.ac.id>
2025-11-21 15:35:10 +0700

    add cors origin

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use rust_app::routes::configure_routes;
 use rust_app::helpers::logger::init_logger;
 use actix_web::{App, HttpServer, middleware::Logger};
 use dotenvy::dotenv;
+use actix_cors::Cors;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -14,7 +15,14 @@ async fn main() -> std::io::Result<()> {
     // run our app with actix web
     tracing::debug!("listening on 0.0.0.0:8080");
     HttpServer::new(|| {
+        let cors = Cors::default()
+            .allow_any_origin()
+            .allow_any_method()
+            .allow_any_header()
+            .max_age(3600);
+
         App::new()
+            .wrap(cors)
             .configure(configure_routes)
             .wrap(Logger::default())
     })
