1c5760e86a956d7985459e6031f1877a1548dfd2
RickyFirmansyah27 <ricky_firmansyah@teknokrat.ac.id>
2025-12-02 10:39:39 +0700

    add tittle refer app

--- src-rs/controllers/open_router_controller.rs
+++ src-rs/controllers/open_router_controller.rs
@@ -2,12 +2,21 @@ use actix_web::{web, HttpResponse, Responder};
 use tracing;
 use serde_json::Value;
 use std::env;
+use std::time::Duration;
+
+const API_URL: &str = "https://openrouter.ai/api/v1/chat/completions";
+const REFERER: &str = "https://online-code-preview.vercel.app";
+const TITLE: &str = "Online Code Preview";
+const TIMEOUT_SECONDS: u64 = 300;
+
 
 pub async fn forward_to_open_router(body: web::Json<Value>) -> impl Responder {
     tracing::info!("Received chat completion request");
 
-    let client = reqwest::Client::new();
-    let open_router_url = "https://openrouter.ai/api/v1/chat/completions";
+    let client = reqwest::Client::builder()
+        .timeout(Duration::from_secs(TIMEOUT_SECONDS))
+        .build()
+        .unwrap();
 
     // Get the API key from an environment variable
     let api_key = match env::var("OPENROUTER_API_KEY") {
@@ -18,11 +27,13 @@ pub async fn forward_to_open_router(body: web::Json<Value>) -> impl Responder {
     };
 
     let res = client
-        .post(open_router_url)
+        .post(API_URL)
         .bearer_auth(api_key)
+        .header("Referer", REFERER)
+        .header("Title", TITLE)
         .json(&body)
         .send()
-            .await;
+        .await;
 
     match res {
         Ok(response) => {
